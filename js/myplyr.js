adc6c51290e2d9f1e5d493d4511bf8f7da09a17b
Juc1 <jr5689@gmail.com>
2021-08-07 10:12:57 +0100

    plyr js
    js

--- js/myplyr.js
+++ js/myplyr.js
@@ -1,2 +1,3 @@
 // single = const player = new Plyr('audio', {});
-const players = Plyr.setup('.plyr-player');
+// const players = Plyr.setup('.plyr-player');
+const players = Array.from(document.querySelectorAll('.js-player')).map(p => new Plyr(p));
